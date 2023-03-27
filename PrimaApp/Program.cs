b96437c794a1febdfac77ca9eae5bada8d28ba77
Reachid <31824032+Reachid@users.noreply.github.com>
2023-03-27 12:35:03 +0200

    Aggiungere i file di progetto.

---
+++ PrimaApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue; 
+        Console.Clear();
+        int slm = int.Parse("1");
+        Console.Write("Inserisci il tuo nome: ");
+        Console.WriteLine("Ciao a te, " + slm + "!");
+
+    }
+}
