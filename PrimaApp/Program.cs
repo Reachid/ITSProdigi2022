35b87093e99e23b8eee896a6241e5e6010c2459f
Reachid <31824032+Reachid@users.noreply.github.com>
2023-03-28 12:49:13 +0200

    Gestione richieste utente aggiunta. Creata la classe Utility

--- PrimaApp/Program.cs
+++ PrimaApp/Program.cs
@@ -1,14 +1,29 @@
-﻿using System.Numerics;
+﻿using PrimaApp;
+using System.Numerics;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.BackgroundColor = ConsoleColor.DarkBlue; 
-        Console.Clear();
-        int slm = int.Parse("1");
-        Console.Write("Inserisci il tuo nome: ");
-        Console.WriteLine("Ciao a te, " + slm + "!");
+        bool richiestaTabellina = true; 
+        while(richiestaTabellina)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
 
-    }
+
+            string richiesta = Utility.chiedi("Che tabellina vuoi?");
+
+            int baseTabellina = int.Parse(richiesta), numeroVolte = Utility.chiediNumero("Quante vuole lo vuoi cliclare? "); ;
+            for (int i = 0; i < numeroVolte + 1; i++)
+            {
+                int risultato = baseTabellina * i;
+                Console.Write($"{baseTabellina}\t*\t{i}\t=\t{risultato}\n");
+            }
+
+            string risposta = Utility.chiedi("Vuoi un'altra tabellina (S/N)? ");
+            richiestaTabellina = risposta == "s"; 
+        }
+        Console.WriteLine("Buona giornata!"); 
+    }   
 }
---
+++ PrimaApp/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaApp
+{
+    internal class Utility
+    {
+        /// <summary>
+        /// funzione per poter porre una domanda all'utente tramite console
+        /// </summary>
+        /// <param name="domanda">La domanda da porre all'utente.</param>
+        /// <returns>Quello che l'utente che ha scritto in console</returns>
+        public static string chiedi(string domanda)
+        {
+            Console.Write(domanda + ": ");
+            string? risposta = Console.ReadLine();
+            return risposta;
+        }
+
+        /// <summary>
+        /// Prende in input una domanda che viene posta all'utente
+        /// </summary>
+        /// <param name="domanda"></param>
+        /// <returns>Ritorna quante volte dev'essere eseguita un'operazione</returns>
+        public static int chiediNumero(string domanda)
+        {
+            Console.Write(domanda + ": ");
+            int risposta = int.Parse(Console.ReadLine());
+            return risposta;
+        }
+    }
+}
